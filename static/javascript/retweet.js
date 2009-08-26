da5b89e9fe9dcf1bbc7bc83b64874e571dfe8588
Benjamin Golub <bgolub@benjamingolub.com>
2009-08-25 20:52:27 -0400

    tweaks

--- static/javascript/retweet.js
+++ static/javascript/retweet.js
@@ -26,7 +26,7 @@ window.RetweetJS = {
 
 	// Tweet Prefix text
 	// "RT @jeresig " would result in: "RT @jeresig Link Title http://bit.ly/asdf"
-	prefix: "",
+	prefix: "RT @bgolub: ",
 
 	// Style information
 	styling: "a.retweet { font: 12px Helvetica,Arial; color: #000; text-decoration: none; border: 0px; }" +
